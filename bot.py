4e2e3ddc22f68e6b2d7c0b6905f3134c44ed36dc
Luís Ribeiro <116073434+LuisRibeiro2903@users.noreply.github.com>
2023-03-09 22:28:07 +0000

    Added miss chance to the Avada Kedavra spell

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 
 
 def run_discord_bot():
-  TOKEN = 'MTA3ODc2MDc5OTQwMjczMzYyOQ.GdoGPm.BCoLudvJ3qKZ_ZQT51DjbA-gkQVRy55QT2455E' #Token is put manually 
+  TOKEN = 'MTA3ODc2MDc5OTQwMjczMzYyOQ.G3vBY5.YqmLXZvAfgWeL_RoQ8oyCuNBk1bKdLC9Zwsn2U' #Token is put manually 
   intents = discord.Intents.all()
   client = commands.Bot(command_prefix = ".", intents = intents)
   links = json.load(open("gifs.json"))
@@ -28,7 +28,7 @@ def run_discord_bot():
       await ctx.send(username + " was hit by ***Expelliarmus*** and is unable to cast magic for " + str(seconds) + " seconds")
       await member.edit(timed_out_until=discord.utils.utcnow() + time)
     else:
-      await ctx.send(ctx.message.author.mention + " tried to use ***Expelliarmus** on " + username + ", but it missed!")
+      await ctx.send(ctx.message.author.mention + " tried to use ***Expelliarmus*** on " + username + ", but it missed!")
 
   @client.command(name = "fin", aliases = ["Finite", "finite", "FINITE"])
   async def End_TO(ctx, member:discord.Member):
@@ -43,9 +43,13 @@ def run_discord_bot():
   @client.command(name = "kill", aliases = ["AVADA_KEDAVRA"])
   @commands.has_any_role(543905846510354442, 1079186787257307228)
   async def kick(ctx, member:discord.Member):
-    await ctx.send(random.choice(links["kill"]))
-    username = member.mention
-    await ctx.send("It can't be. " + ctx.message.author.mention + " used the killing curse :fearful: Poor " + username)
-    await member.kick(reason="AVADA KEDAVRA")
+    percem = random.randint(1,10)
+    if percem <= 2:
+      await ctx.send(random.choice(links["kill"]))
+      username = member.mention
+      await ctx.send("It can't be. " + ctx.message.author.mention + " used the killing curse :fearful: Poor " + username)
+      await member.kick(reason="AVADA KEDAVRA")
+    else:
+      await ctx.send(ctx.message.author.mention + " tried to use ***Avada Kedavra*** on " + username + ", but it missed!")
   client.run(TOKEN)
     
